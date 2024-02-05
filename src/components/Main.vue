1aa337fa2321dfd264c1e139ba4ffc5447e421ad
BroMaster132 <emirmatov@gmail.com>
2024-02-05 13:42:18 +0600

    Maybe finished

---
+++ src/components/Main.vue
@@ -0,0 +1,100 @@
+<script setup>
+$(document).ready(function(){ 
+    const cryingCat = '../src/assets/cryingCat.gif'
+    const btnNo = $(`#btnNo`)
+    const btnYes = $(`#btnYes`)
+    const cat = $(`#cat`)
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function btnTeleport() {
+        let random = getRandomInt(-600,600)
+        let randomSec = getRandomInt(-600,600)
+        btnNo.css({
+            'left': random + 'px',
+            'right': random  + 'px',
+            'top' : randomSec  + 'px',
+            'bottom' : randomSec + 'px',
+        })
+        cat.attr('src', cryingCat)
+    }
+
+    btnNo.mouseenter(() => {
+        btnTeleport()
+    })
+    btnNo.click(() =>{
+        btnTeleport()
+    })
+
+    btnYes.click(() => {
+        cat.attr('src','../assets/kawai.gif')
+    })
+})
+</script>
+
+<template>
+  <div class="middle">
+    <br>
+    <h1>Will you marry me?</h1>
+    <img id="cat" src="../assets/kawai.gif"> 
+    <div class="buttons">
+        <button id="btnYes"> Yes</button>
+        <button id="btnNo" style="margin-left: 90px ;"> No</button>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.middle img{
+  position: relative;
+  left: 40%;
+  height: 500px;
+}
+@font-face {
+  font-family: beer ;
+  src: url(./src/assets/beer\ money.ttf)
+
+} 
+#cry {
+  display: none;
+}
+.middle h1 {
+  text-align: center;
+  font-family: beer;
+  font-size: 45pt;
+}
+
+
+.buttons {
+  height: 200px;
+  width: 300px;
+  position: relative;
+  left: 44%;
+  padding-top: 50px;
+}
+
+.buttons button {
+  position: relative;
+  background-color: #272727;
+  border: none;
+  color: white;
+  padding: 20px 37px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 15px;
+  cursor: pointer;
+  transition: 1s;
+  font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+.buttons button:hover {
+  scale: 1.1;
+}
+
+
+</style>
